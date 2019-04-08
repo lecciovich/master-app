7cf060f281a4ec59965f7fbda6b2f3b43101746d
support321 <supporto@321.it>
2019-04-08 19:52:49 +0200

    prova di modellazione reale

--- newmod/models/models.py
+++ newmod/models/models.py
@@ -2,14 +2,6 @@
 
 from odoo import models, fields, api
 
-# class newmod(models.Model):
-#     _name = 'newmod.newmod'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
@@ -17,21 +9,34 @@ from odoo import models, fields, api
 class CourseCalendar(models.Model):
     """Model for course calendar
     """
-    _name = 'GestCal.course'
+    _name = 'gestcal.date'
     _description = ""
     _inherit = "calendar.event"
     _order = "" ## Ordering field when searching without an ordering specified (default: 'id') 
 
-    proj = fields.Many2one()
+    date = fields.Date()
+    start_time = fields.Datetime()
+    end_time = fields.Datetime()
+    place = fields.Many2one('gestcal.place') ## (comodel_name=<object object>, inverse_name=<object object>, string=<object object>, **kwargs)
+                                              ## comodel c'è, inverse name bisogna fare la classe per le sedi, il resto non so manco cos'è
+    beneficiaries = fields.Many2one('res.user')  ## oppure users che beneficiaries è lungo e tedioso
+    docent = fields.Many2one('res.user')
+    course_module = fields('gestcal.module')
+
+
+class CourseModule(models.Model):
+    """Model for the courses modules in a project
+    """
+    _name = 'gestcal.coursemod'
+    _description = ""
+    #_inherit = "" ## anche niente da ereditare
+    #_order = "" ## boh
+
+    title = fields.Text()
+    #recurrence = ## da decidere cosa fare
+    tot_hours = fields.Float()
+    courses_date = fields.Many2one('gestcal.date') ## non so cosa sto facendo, sarebbe che ogni edizione potesse avere la view delle date delle lezioni
+    ufc = fields.Tex()
+    method = fields.Tex()
+
     
-
-
-
-# CAMPI
-# data (datetime)
-# ora inizio (datetime/float)
-# ora fine (datetime/float)
-# sede (string/ ref sedi?)
-# ** destinatari (ref anagrafe)
-# ** modulo (ref moduli)
-# ** docente (ref contatto)
