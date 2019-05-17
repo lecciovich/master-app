68c0b25395efe32cbfc60871de8537ccc29e8533
pc <pc@pc-PC>
2019-05-17 19:09:18 +0100

    add constrains for lesson date

--- gest_call/models/gest_call.py
+++ gest_call/models/gest_call.py
@@ -73,7 +73,7 @@ class GestcalLesson(models.Model):
 #     _inherit = "calendar.event"
 
     title = fields.Char(string="Title", required=True)
-    date = fields.Date(string="Date")
+    date = fields.Date(string="Date", required=True)
     start_time = fields.Float('Start Time', required=True,
                               help="Time according to timeformat of 24 hours")
     end_time = fields.Float('End Time', required=True,
@@ -82,6 +82,50 @@ class GestcalLesson(models.Model):
     beneficiaries_id = fields.Many2many('res.partner','lesson_id', string="Beneficiaries") # shouldn't this be One2many? One lession has many beneficiaries (or recipients)
     course_id = fields.Many2one('gestcal.course', string="course", required=True)
     project_id = fields.Many2one('gestcal.project', string="Project")
+
+
+#     @api.one 
+#     @api.constrains('date','start_time','end_time','teacher_id')
+#     def _check_class_semestre_year(self):
+#         print ('const')
+#         cls=self.env['gestcal.lesson']
+#         start = self.start_time
+#         end = self.end_time
+#         print ('start',start)
+#         print ('end',end)
+#         check11=cls.search([('start_time','=',self.start_time)])
+#         print ('check11',check11)
+#         check=cls.search([('date','=',self.date),('start_time','=',self.start_time),('teacher_id','=',self.teacher_id.id)])
+#         print ('tt',check)
+#         if len(check)>1:
+#             raise ValidationError(_("This date already exists"))
+
+    @api.constrains('date','start_time','end_time','teacher_id')
+    def cheking_lesson(self): 
+  
+        for record in self:
+            date = self.search([('date','=',record.date),('id','!=',record.id)])
+            logger.info("___________date________: %s  ",date,record.date)
+            
+            date_st = self.search([('start_time','=',record.start_time),('id','!=',record.id)])
+            logger.info("___________date_st________: %s  ",date_st)
+            
+            date_ed = self.search([('end_time','=',record.end_time),('id','!=',record.id)])
+            logger.info("___________date_ed________: %s  ",date_ed)
+            
+            teacher = self.search([('teacher_id','=',record.teacher_id.id),('id','!=',record.id)])
+            logger.info("___________teacher________: %s  ",teacher)
+              
+            if date and date_st and date_ed and teacher: 
+                raise ValidationError(_("This date already exists for the lesson"))
+
+    @api.constrains('start_time','end_time')
+    def check_date(self):
+        if (self.start_time >= self.end_time):
+            logger.info("___________check_date________: %s  ",self.start_time > self.end_time)
+            raise ValidationError (_("Start time must be greater than end time !")) 
+    
+
 
 class GestcalEquipment(models.Model):
 
