1259ee6d46f209641a30e2507a38e534159f5c7d
Boubaker AB <pc@pc-PC>
2019-07-23 00:10:57 +0100

    correction for check method

--- gest_call/models/lesson.py
+++ gest_call/models/lesson.py
@@ -5,15 +5,12 @@ from odoo.exceptions import ValidationError
 import logging
 logger=logging.getLogger('_______LOGGER B____________')
 
-
-
 class GestcalLesson(models.Model):
 
     _name = 'gestcal.lesson'
     _rec_name = 'date'
     _description = 'Gestcal lesson'
 #     _inherit = 'calendar.event'
-
 
     date = fields.Date(string='Date', required=True)
     start_time = fields.Float(string='Start Time', required=True,
@@ -27,27 +24,37 @@ class GestcalLesson(models.Model):
     place = fields.Many2one('gestcal.place', string='Place')
 
 
+#     @api.constrains('date','start_time','end_time','teacher_id')
+#     def cheking_lesson(self): 
+#   
+#         for record in self:
+#             logger.info('__________record________: %s  ',record)
+#             date = self.search([('date','=',record.date),('id','!=',record.id)])
+#             logger.info('___________date________: %s  ',date)
+#             
+#             date_st = self.search([('start_time','=',record.start_time),('id','!=',record.id)])
+#             logger.info('___________date_st________: %s  ',date_st)
+#             
+#             date_ed = self.search([('end_time','=',record.end_time),('id','!=',record.id)])
+#             logger.info('___________date_ed________: %s  ',date_ed)
+#             
+#             teacher = self.search([('teacher_id','=',record.teacher_id.id),('id','!=',record.id)])
+#             logger.info('___________teacher________: %s  ',teacher)
+#               
+#             if date and date_st and date_ed and teacher: 
+#                 raise ValidationError(_('This date already exists for the lesson'))
+
+    @api.one
     @api.constrains('date','start_time','end_time','teacher_id')
-    def cheking_lesson(self): 
+    def cheking_lesson(self):
+        lesson_obj = self.env['gestcal.lesson']
   
-        for record in self:
-            logger.info('__________record________: %s  ',record)
-            date = self.search([('date','=',record.date),('id','!=',record.id)])
-            logger.info('___________date________: %s  ',date)
-            
-            date_st = self.search([('start_time','=',record.start_time),('id','!=',record.id)])
-            logger.info('___________date_st________: %s  ',date_st)
-            
-            date_ed = self.search([('end_time','=',record.end_time),('id','!=',record.id)])
-            logger.info('___________date_ed________: %s  ',date_ed)
-            
-            teacher = self.search([('teacher_id','=',record.teacher_id.id),('id','!=',record.id)])
-            logger.info('___________teacher________: %s  ',teacher)
-              
-            if date and date_st and date_ed and teacher: 
+        for rec in lesson_obj.search([]):
+            check_less = rec.search([('date' ,'=',self.date),('start_time' ,'=', self.start_time),('end_time' ,'=', self.end_time),('teacher_id' ,'=', self.teacher_id.id)])
+            if len(check_less) > 1 :
                 raise ValidationError(_('This date already exists for the lesson'))
 
-
+        
     @api.constrains('start_time','end_time')
     def check_date(self):
         if (self.start_time >= self.end_time):
