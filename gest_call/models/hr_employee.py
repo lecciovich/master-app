591ebcbdd05137fdb22efba69ab90d301e742abb
odoovm <Lecce Marco>
2020-06-01 00:37:56 +0200

    added multiselect topic to teacher employee

--- gest_call/models/hr_employee.py
+++ gest_call/models/hr_employee.py
@@ -4,7 +4,17 @@ from odoo import models, fields, api, _
 class HrEmployee(models.Model):
 
     _inherit = 'hr.employee'
+    _rec_name = 'name'
     
     lesson_id = fields.Many2one('gestcal.lesson', 'Lessons')
     is_teacher = fields.Boolean('Is Teacher')
+    topics = fields.Many2many('hr.topic', string='Topics')
+
+
+class Topic(models.Model):
+
+    _name = 'hr.topic'
+    _rec_name = 'title'
+
+    title= fields.Char("Title")
 
