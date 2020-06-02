839bbfdcaa01b858b32607d50a4e740ba14fb75f
odoovm <Lecce Marco>
2020-06-02 18:57:35 +0200

    Set model reference to topics in gestcall.py

--- gest_call/models/hr_employee.py
+++ gest_call/models/hr_employee.py
@@ -8,13 +8,13 @@ class HrEmployee(models.Model):
     
     lesson_id = fields.Many2one('gestcal.lesson', 'Lessons')
     is_teacher = fields.Boolean('Is Teacher')
-    topics = fields.Many2many('hr.topic', string='Topics')
-
-
-class Topic(models.Model):
-
-    _name = 'hr.topic'
-    _rec_name = 'title'
-
-    title= fields.Char("Title")
-
+    topics = fields.Many2many('gestcal.course.topics', string='Topics')
+#
+#
+# class Topic(models.Model):
+#
+#     _name = 'hr.topic'
+#     _rec_name = 'title'
+#
+#     title= fields.Char("Title")
+#
