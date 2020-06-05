f82ac6dd04bc697599a4457e17c36fbe1eb21844
odoovm <Lecce Marco>
2020-06-05 11:51:09 +0200

    course notebook page teacher now editable

--- gest_call/models/course.py
+++ gest_call/models/course.py
@@ -19,7 +19,7 @@ class GestcalCourse(models.Model):
     name = fields.Char(string='Name')
     repetition = fields.Char(string='Repetition')
     total_hours = fields.Float(string='Total Hours')
-    topics = fields.Many2many('gestcal.course.topics',string='Theme Areas', store=True)
+    topics = fields.Many2many('gestcal.course.topics', 'name', string='Theme Areas',readonly=False)#, store=True
     lesson_id = fields.One2many('gestcal.lesson','course_id', string='Lesson') 
     attachments_ids = fields.One2many('gestcal.attachment', 'courses_id', string='Attachment')
     attachment_count = fields.Integer(compute='_compute_attachment_count', string='Attachment count')
